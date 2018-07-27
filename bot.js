c1e4282821760731b1cee298ae68a69ef0e51258
7S0 <41790254+7S0@users.noreply.github.com>
2018-07-27 16:54:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- const prefix = "برفكسك";
+ const prefix = "$";
 client.on('ready', () => {
     console.log('I am ready!');
 });
